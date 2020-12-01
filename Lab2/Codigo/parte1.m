449b23e6566d892d99a65e6fcb2a0731281e3981
chun-zen yu <chunzen1998@gmail.com>
2020-12-01 00:47:22 -0300

    polos ceros staticgain settlingtime

--- Lab2/Codigo/parte1.m
+++ Lab2/Codigo/parte1.m
@@ -1,50 +1,73 @@
 clear,clc
 
-%Primera parte de laboratorio
+% Primera parte de laboratorio
 
-%ADVERTENCIA: Se dejaron todos los graficos (step) comentados, si se desean
-%ver se deben descomentar
+% Se explica como obtener los gráficos de las respuestas de lazo abierto 
+% y cerrado de las siguientes funciones
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Funcion 1 %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Se explica como obtener los gráficos de las respuestas de lazo abierto
-%y cerrado de las siguientes funciones
+% 6*y'(t) + 2*y(t) = 8*u'(t)
+% Despues se utilizada la tranformada de Laplace, dejando la función
+% expresada en el parametro s. Realizando una serie de calculos la funcion
+% de tranferencia quedaría:
+% Y(s) = 4*s*u(s) / (3*s+1)
 
-%%%%%%Funcion 1%%%%%%
-
-%6*y'(t) + 2*y(t) = 8*u'(t)
-%Despues se utilizada la tranformada de Laplace, dejando la función
-%expresada en el parametro s. Realizando una serie de calculos la funcion
-%de tranferencia quedaría:
-%Y(s) = 4*s*u(s) / (3*s+1)
-
-%Definimos el numerador y denominador de la función de transferencia
+% Definimos el numerador y denominador de la función de transferencia
 num1= [4,0];
 den1= [3,1];
-%Se utiliza tf para definir la función de trasnferencia
+% Se utiliza tf para definir la función de trasnferencia
 s1 = tf(num1,den1);
 
-%Gráfico de lazo abierto
-%step(s1)
-%title("Función de transferencia 1: Lazo Abierto");
-%grid on
+% Para obtener los polos y zeros de la funcion
+[s1_z, s1_p, s1_k] = zpkdata(s1);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s1_estab = stepinfo(s1);
+
+% Gráfico de lazo abierto
+figure(1)
+step(s1);
+title("Función de transferencia 1: Lazo Abierto");
+grid on;
+
 
 %Utilizando la función 'feedback' se calcula el lazo cerrado
 %Se coloca 1 como H2 porque el feedback entre dos funciones debe ser el
 %mismo que si fuera solo una función (el cual es este caso).
-s_cerrado = feedback(s1,1);
+s1_cerrado = feedback(s1,1,-1);
+
+
+% Para obtener los polos y zeros de la funcion
+[s1_cerrado_z, s1_cerrado_p, s1_cerrado_k] = zpkdata(s1_cerrado);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s1_cerrado_estab = stepinfo(s1_cerrado);
 
 %Gráfico de lazo cerrado
-%step(s_cerrado)
-%title("Función de transferencia 1: Lazo Cerrado");
-%grid on
+figure(2)
+step(s1_cerrado);
+title("Función de transferencia 1: Lazo Cerrado");
+grid on
 
-%%%%%%Funcion 2%%%%%%
+% Grafico de polos y zeros lazo abierto y cerrado
+figure(5)
+title("Zeros y polos de función de transferencia 1");
+pzmap(s1,"b", s1_cerrado,"r")
+legend("abierto", "cerrado")
+grid on;
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Funcion 2 %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %y''(t) + 6*y'(t) + 3*y(t) = 5*u''(t) + 7*u'(t) + 4*u(t)
 %Despues se utilizada la tranformada de Laplace, dejando la función
 %expresada en el parametro s. Realizando una serie de calculos la funcion
 %de tranferencia quedaría:
-%Y(s) = (5*s² + 7*s + 4) / (s² + 6*s)
+%Y(s) = (5*s² + 7*s + 4) / (s² + 6*s + 3)
 
 %Definimos el numerador y denominador de la función de transferencia
 num2= [5,7,4];
@@ -52,27 +75,35 @@ den2= [1,6,3];
 %Se utiliza tf para definir la función de trasnferencia
 s2 = tf(num2,den2);
 
+% Para obtener los polos y zeros de la funcion
+[s2_z, s2_p ,s2_k] = zpkdata(s2);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s2_estab = stepinfo(s2);
+
 %Gráfico de lazo abierto
-%step(s2)
-%title("Función de transferencia 2: Lazo Abierto");
-%grid on
+figure(3)
+step(s2)
+title("Función de transferencia 2: Lazo Abierto");
+grid on
 
-s_cerrado2 = feedback(s2,1);
+s2_cerrado = feedback(s2,1,-1);
+
+% Para obtener los polos y zeros de la funcion
+[s2_cerrado_z, s2_cerrado_p , s2_cerrado_k] = zpkdata(s2_cerrado);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s2_cerrado_estab = stepinfo(s2_cerrado);
+
 %Gráfico de lazo cerrado
-%step(s_cerrado2)
-%title("Función de transferencia 2: Lazo Cerrado");
-%grid on
+figure(4);
+step(s2_cerrado);
+title("Función de transferencia 2: Lazo Cerrado");
+grid on
 
-%Funcion 3
-
-%Dale chun inventa alguno
-
-
-
-
-
-
-
-
-
-
+% Grafico de polos y zeros lazo abierto y cerrado
+figure(6)
+title("Zeros y polos de función de transferencia 2");
+pzmap(s2,"b", s2_cerrado,"r")
+legend("abierto", "cerrado")
+grid on;
--- Lab2/Codigo/parte2.m
+++ Lab2/Codigo/parte2.m
@@ -12,14 +12,22 @@ H5 = (5*s + 3)/(5*s^3 + s^2 + 4);
 H6 = (5*s + 1)/(s^3 + 6*s^2 + 5);
 
 %Conexiones del sistema
-A1 = feedback(H3,1)
-A2 = parallel(H4,H5)
-A3 = series(A1,A2)
-A4 = series(A3,H6)
-A5 = parallel(H1,H2)
-final = parallel(A5,A4)
+% A1 = feedback(H3,1,1)
+% A2 = parallel(H4,H5)
+% A3 = series(A1,A2)
+% A4 = series(A3,H6)
+% A5 = parallel(H1,H2)
+% final = parallel(A5,A4)
+
+% Operaciones para obtener la funcion de transferencia final
+B1 = H1 + H2;
+B2 = feedback(H3,1,1);
+B3 = H4 + H5;
+
+final = B1 + (H6 * B3 * B2);
 
 
+figure(6)
 step(final)
 title("Respuesta al escalón");
 grid on
